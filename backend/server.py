58de51ff2689b7cb1dd07e0bb5634fa0aa9286e3
Anon <opss59332@gmail.com>
2024-03-13 10:00:55 +0530

    basic functionalities done

---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+
+app = Flask(__name__)
+CORS(app)
+
+pattern = re.compile(r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})')
+
+def getVideoId(url):
+    match = pattern.search(url)
+    if match:
+        return match.group(1)
+    return None
+
+@app.route('/transcript')
+def get_data():
+    url = request.args.get('video_url')
+    videoId = getVideoId(url)
+    transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
+    for transcript in transcript_list:
+        return jsonify(transcript.fetch())
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/script.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let searchBtnEl = document.getElementById("searchBtn");
+  let dataContainerEl = document.getElementById("dataContainer");
+  let videoInputEl = document.getElementById("inputVideoUrl");
+  let searchInputEl = document.getElementById("searchInput");
+  let formEl = document.getElementById("form");
+
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      let url = tabs[0].url;
+      videoInputEl.value = url;
+    });
+
+  function formatTime(timeInSeconds) {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = (timeInSeconds % 3600) % 60;
+
+    return `${hours ? hours + "h:" : ""}${minutes ? minutes + "min : " : ""}${seconds + "sec"}`;
+  }
+
+  function extractVideoId(url) {
+    const pattern =
+      /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(pattern);
+    if (match) {
+      return match[1];
+    }
+    return null;
+  }
+
+  formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetchDataFromBackend();
+  });
+
+  function search(dataFromBackend) {
+    let filteredArr = [];
+    dataContainerEl.innerHTML = "";
+    dataFromBackend.map((transcript) => {
+      if (
+        transcript.text
+          .toLowerCase()
+          .includes(searchInputEl.value.toLowerCase())
+      ) {
+        filteredArr.push(transcript);
+        console.log(transcript.text);
+      }
+    });
+    filteredArr.map((result) => {
+      const videoId = extractVideoId(videoInputEl.value);
+
+      const pStartTime = document.createElement("a");
+      pStartTime.setAttribute("id", "start-time-a");
+      pStartTime.href = `${
+        "https://youtube.com/watch?v=" +
+        videoId +
+        "&t=" +
+        Math.floor(result.start) +
+        "s"
+      }`;
+      pStartTime.target = "_blank";
+      pStartTime.innerHTML = formatTime(Math.floor(result.start))
+
+      const pText = document.createElement("p");
+      pText.setAttribute("id", "p-text");
+      pText.innerHTML = result.text;
+
+      const div = document.createElement("div");
+      div.setAttribute("id", "groupDiv");
+      div.appendChild(pStartTime);
+      div.appendChild(pText);
+
+      dataContainerEl.appendChild(div);
+
+      searchBtnEl.innerHTML = "Search";
+      searchBtnEl.disabled = false;
+    });
+  }
+
+  function fetchDataFromBackend() {
+    searchBtnEl.innerHTML = "Searching...";
+    searchBtnEl.disabled = true;
+    fetch(
+      `http://localhost:5000/transcript?video_url=${encodeURIComponent(
+        videoInputEl.value
+      )}`
+    )
+      .then((data) => data.json())
+      .then((resp) => {
+        search(resp);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+});
